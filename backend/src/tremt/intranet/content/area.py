d958d1910e0a8eee0e405329e876d72aafb51aa4
Luiz Carlos Pereira <lcpgeop@gmail.com>
2025-09-26 13:31:19 -0400

    Exercício 28: Vocabulário de estados

--- backend/src/tremt/intranet/content/area.py
+++ backend/src/tremt/intranet/content/area.py
@@ -2,12 +2,13 @@ from plone.dexterity.content import Container
 from plone.schema.email import Email
 from plone.supermodel import model
 from tremt.intranet import _
+from tremt.intranet.utils import validadores
 from zope import schema
 from zope.interface import implementer
 
 
 class IArea(model.Schema):
-    """Definicao de uma Area."""
+    """Definição de uma Área."""
 
     model.fieldset(
         "contato",
@@ -20,15 +21,55 @@ class IArea(model.Schema):
     email = Email(
         title=_("Email"),
         required=True,
+        constraint=validadores.is_valid_email,
     )
 
     telefone = schema.TextLine(
         title=_("Telefone"),
         description=_("Informe o telefone de contato"),
         required=False,
+        constraint=validadores.is_valid_telefone,
+    )
+
+    model.fieldset(
+        "endereco",
+        _("Endereço"),
+        fields=[
+            "endereco",
+            "complemento",
+            "cidade",
+            "estado",
+            "cep",
+        ],
+    )
+    endereco = schema.TextLine(
+        title=_("Endereço"),
+        required=False,
+        default="",
+    )
+    complemento = schema.TextLine(
+        title=_("Complemento"),
+        description=_("Ex. Anexo, Sala"),
+        required=False,
+        default="",
+    )
+    cidade = schema.TextLine(
+        title=_("Cidade"),
+        required=False,
+        default="",
+    )
+    estado = schema.Choice(
+        title=_("Estado"),
+        vocabulary="tremt.intranet.vocabulary.estados",
+        required=False,
+    )
+    cep = schema.TextLine(
+        title=_("CEP"),
+        required=False,
+        default="",
     )
 
 
 @implementer(IArea)
 class Area(Container):
-    """Uma Area no TRE-MT."""
+    """Uma Área no TRE-MT."""
